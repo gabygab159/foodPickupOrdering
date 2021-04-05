0ae9adad4ff7ef38b394fc79d0a6eb6b9e0a34ec
Sam <sribas@gmail.com>
2021-04-05 19:04:50 +0000

    eslint helper-functions folder

--- helper-functions/twilio.js
+++ helper-functions/twilio.js
@@ -9,17 +9,17 @@ const client = new twilio(accountSid, authToken);
 const sendSMS = (phone, message) => {
 
   return client.messages.create({
-      body: message,
-      to: phone,  // Text this number
-      from: '+18142088997' // From a valid Twilio number
+    body: message,
+    to: phone,  // Text this number
+    from: '+18142088997' // From a valid Twilio number
   })
-  .then((message) => {
-    return message.sid
-  })
-  .catch((err) => {
-    return err.messages
-  })
+    .then((message) => {
+      return message.sid;
+    })
+    .catch((err) => {
+      return err.messages;
+    });
 
-}
+};
 
 module.exports = sendSMS;
