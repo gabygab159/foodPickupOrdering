244d508e7fa2d798a939d34bcdbf6ee48973d58c
Sam <sribas@gmail.com>
2021-04-05 21:25:01 +0000

    Add messagesRoute and fix usersRoute module call

--- server.js
+++ server.js
@@ -31,14 +31,14 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoute = require("./routes/users-routes");
 const menusRoute = require("./routes/menus-routes");
-const messageRoute = require("./routes/messages-routes");
+const messagesRoute = require("./routes/messages-routes");
 
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use('/users', usersRouter(database));
+app.use('/users', usersRoute(database));
 app.use('/menus', menusRoute(database));
-app.use('/messages', messageRoute(database));
+app.use('/messages', messagesRoute(database));
 
 // Note: mount other resources here, using the same pattern above
 
