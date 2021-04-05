8cd13a03b05570855119c9b6c0cecfe19a89d3bf
Gabriel <gabriel.jchen@gmail.com>
2021-04-05 15:11:04 +0000

    start creating seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,6 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (first_name, last_name, email, phone_number, password, address_id )
+VALUES ('Isaac', 'Asimov', 'iasimov@hotmail.com', 3212225456, 'foundation', 1),
+('Frank', 'Herbert', 'muadib@gmail.com', 5455557845, 'arakis', 2),
+('Ursula', 'Le Guin', 'ursula@gmail.com', 9698754254), 'lefthand', 3;
+
---
+++ db/seeds/02_addresses.sql
@@ -0,0 +1,5 @@
+INSERT INTO addresses (street, city, postal_code, province, country)
+VALUES ('123 Circle', 'Montreal', 's1s1s1', 'Quebec', 'Canada'),
+('456 Square', 'Montreal', 'd3d3d3', 'Quebec', 'Canada'),
+('789 Triangle', 'Montreal', 'f5f5f5', 'Quebec', 'Canada'),
+('961 Rectangle', 'Montreal', 'j6j6j6', 'Quebec', 'Canada');
