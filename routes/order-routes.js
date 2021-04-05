a7846f324f4ebf537c870de15a1ad2732e8eebb8
Gabriel <gabriel.jchen@gmail.com>
2021-04-05 22:36:49 +0000

    routes for /orders and /orders/:id in place need to test with db

---
+++ routes/order-routes.js
@@ -0,0 +1,41 @@
+// All routes for Orders are defined here
+
+
+const express = require('express');
+const router  = express.Router();
+const { getOrders, getOrderById }  = require('../lib/orders-queries');
+
+// Router middlewares with no mount path (will be executed on every request to the router)
+router.use((req, res, next) => {
+  console.log('router.user has been called');
+  next();
+});
+
+module.exports = (database) => {
+  // GET /orders/
+  router.get('/', (req,res) => {
+    getOrders()
+      .then((orders) => {
+        res.send(orders);
+      })
+      .catch((err) => {
+        return err.messages;
+      })
+  })
+
+  router.get('/:id', (req, res) => {
+   if (req.params.id) {
+     getOrderById(req.params.id)
+      .then((menus) => {
+        res.send(menus);
+      })
+      .catch((err) => {
+        res.send(err.messages);
+      })
+    }
+  })
+
+  return router;
+}
+
+
