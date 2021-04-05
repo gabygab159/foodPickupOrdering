119fbf5a9a13c8b5b6bd38c58d0c3d273a06d7d2
Sam <sribas@gmail.com>
2021-04-05 11:57:42 +0000

    Add modules, some middleware, and database to server.js

--- server.js
+++ server.js
@@ -4,9 +4,27 @@ const express = require('express');
 const router = express.Router();
 
 const morgan = require('morgan');
+const bodyParser = require("body-parser");
+const sass       = require("node-sass-middleware");
+const database   = require('./lib/db');
 
 const port = process.env.PORT || 8080;
 const app = express();
+
+app.use(morgan('dev'));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/styles", sass({
+  src: __dirname + "/styles",
+  dest: __dirname + "/public/styles",
+  debug: true,
+  outputStyle: 'expanded'
+}));
+app.use(express.static("public"));
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello');
