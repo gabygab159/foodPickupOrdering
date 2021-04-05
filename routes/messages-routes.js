511e40246579c094c30f113c805cbe9ae6c05592
Sam <sribas@gmail.com>
2021-04-05 21:12:52 +0000

    Add routes/messages-routes.js

---
+++ routes/messages-routes.js
@@ -0,0 +1,43 @@
+/*
+ * All routes for Manus are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+const express = require('express');
+const router  = express.Router();
+const { getMenuItems, getMenuItemsById }  = require('../lib/menus-queries');
+
+// Router middlewares with no mount path (will be executed on every request to the router)
+router.use((req, res, next) => {
+  console.log('router.menus has been called');
+  next();
+})
+
+module.exports = (database) => {
+  // GET /menus/
+  router.get('/', (req, res) => {
+    getMenuItems()
+      .then((menus) => {
+        res.send(menus);
+      })
+      .catch((err) => {
+        return err.messages;
+      })
+  })
+
+  // GET /menus/:id
+  router.get('/:id', (req, res) => {
+    if (req.params.id) {
+      getMenuItemsById(req.params.id)
+        .then((menus) => {
+          res.send(menus);
+        })
+        .catch((err) => {
+          res.send(err.messages);
+        })
+    }
+  })
+
+  return router;
+}
