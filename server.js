0878d11b90143f16cafffdb39555aa40e1e93074
Sam <sribas@gmail.com>
2021-04-06 16:25:54 +0000

    Add ejs templates for cart and messsages sections

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use('/order-items', orderItemsRoute(database));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("pages/index");
 });
 
 app.listen(PORT, () => {
