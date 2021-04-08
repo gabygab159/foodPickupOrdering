3b50047cbc4f98b4638cd4d2893eee4991bf5f38
Sam <sribas@gmail.com>
2021-04-08 16:53:26 +0000

    Add comments with the info the cart form is sending to the /checkouts routing

--- routes/checkout-routes.js
+++ routes/checkout-routes.js
@@ -15,31 +15,39 @@ module.exports = (database) => {
   router.post('/', (req, res) => {
 
     console.log("DATA COMING FROM THE CHECKOUT: ", req.body);
-    res.send(req.body);
+    // res.send(req.body);
+    // checkout form send this
+    // {
+    //   user_id: "1",
+    //   order_id: "7",
+    //   restaurant_id: "1",
+    //   total: "2373",
+    //   prep_time: "12"
+    // }
 
 
-    // updateOrderStatus(9, 2)
-    //   .then((status) => {
-    //     //We need to send the restaurant with order id
-    //     sendSMS("+15149635280", "restaurant order with id")
-    //       .then(res => {
-    //         //send to client initial sms of order # and time
-    //         sendSMS("+14372421211", "client order message")
-    //           .then(res=> {
-    //             //set timeout. send client order ready for pickup
-    //             console.log('message sent to client')
-    //             // inject the client phone number and the time
-    //             setTimeout(sendSMS("+14372421211", "Order is ready for pickup!"),10000)
-    //           })
-    //           .catch(e=>console.error('did not send to client', e))
-    //         })
-    //       .catch(e => console.error("twilio error",e));
-    //     res.send(status)
-    //   })
-    //   .catch((err) => {
-    //     console.error(err);
-    //     res.status(500).json(err);
-    //   });
+    updateOrderStatus(9, 2)
+      .then((status) => {
+        //We need to send the restaurant with order id
+        sendSMS("+15149635280", "restaurant order with id")
+          .then(res => {
+            //send to client initial sms of order # and time
+            sendSMS("+14372421211", "client order message")
+              .then(res=> {
+                //set timeout. send client order ready for pickup
+                console.log('message sent to client')
+                // inject the client phone number and the time
+                setTimeout(sendSMS("+14372421211", "Order is ready for pickup!"),10000)
+              })
+              .catch(e=>console.error('did not send to client', e))
+            })
+          .catch(e => console.error("twilio error",e));
+        res.send(status)
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).json(err);
+      });
 
 
   });
