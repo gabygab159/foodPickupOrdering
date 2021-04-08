ac99ffe80caf50bbd1e7277061a3f7ecfaf67fb0
Sam <sribas@gmail.com>
2021-04-08 19:43:20 +0000

    fix bugs

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -32,6 +32,7 @@ module.exports = (database) => {
   router.get('/', (req, res) => {
   
     const user_id = 1;
+    const restaurant_id = 1;
 
     // Get all dishes from the menu and render menu_items page with all objects from the menu_items table
     getMenuItems()
@@ -41,55 +42,65 @@ module.exports = (database) => {
         getAddressesById(restaurant_id)
         .then((address) => {
           templateVars.addresses = address;
+
+
+
           getRestaurantById(restaurant_id)
           .then((restaurant) => {
 
             templateVars.restaurants = restaurant;
 
-            getUsersById(user_id)
+            getUsersById(user_id) 
             .then((users) => {
+              templateVars.users = users;
 
-              templateVars.users = users
-
-
-        getOrdersByUserId(user_id)
-          .then((userOrder) => {
-            // if we get the menu items
-            // we check if there are orders for the user
-            console.log("getOrdersByUserId inside index-route.js: ", userOrder);
-            if(!userOrder) {
-              //console.log("USERORDER (no orders for the user): ", userOrder);
-              //orderItems = [];
-              templateVars.orderOpen = userOrder;
-              templateVars.orderActive = userOrder;
-              res.render('pages/index', templateVars);
-            } else {
-              //console.log(">>>>>>USERORDER EXIST: ", userOrder);
-              // There is an order to the user with status 1 or 2
-              // if status = 1, get order items
-              //console.log("USER ORDERS BEFORE GETTING ITEMS: ", userOrder);
-                templateVars.orderOpenInfo = userOrder.filter(o => o.status === 1);
-                templateVars.orderActiveInfo = userOrder.filter(a => a.status === 2);
-                getOrderItems(user_id)
-                .then((orders) => {
-                  let orderActive = false;
-              
-                  // if order status = 1 -> show items in the cart
-                  // if order status = 2 -> show order info in the message, and messages
-
-                  templateVars.orderOpen = orders.filter(o => o.status === 1); // This variable needs to be renamed to orderOpenItems
-                  templateVars.orderActive = userOrder.filter(a => a.status === 2 ? a.id : false); // This variable need to be renamed to orderActiveItem
-
+              getOrdersByUserId(user_id)
+                .then((userOrder) => {
+                  // if we get the menu items
+                  // we check if there are orders for the user
+                  console.log("getOrdersByUserId inside index-route.js: ", userOrder);
+                  if(!userOrder) {
+                    //console.log("USERORDER (no orders for the user): ", userOrder);
+                    //orderItems = [];
+                    templateVars.orderOpen = userOrder;
+                    templateVars.orderActive = userOrder;
                     res.render('pages/index', templateVars);
-                  })
-            }
+                  } else {
+                    //console.log(">>>>>>USERORDER EXIST: ", userOrder);
+                    // There is an order to the user with status 1 or 2
+                    // if status = 1, get order items
+                    //console.log("USER ORDERS BEFORE GETTING ITEMS: ", userOrder);
+                    templateVars.orderOpenInfo = userOrder.filter(o => o.status === 1);
+                    templateVars.orderActiveInfo = userOrder.filter(a => a.status === 2);
+                    getOrderItems(user_id)
+                    .then((orders) => {
+                      let orderActive = false;                   
+                      // if order status = 1 -> show items in the cart
+                      // if order status = 2 -> show order info in the message, and messages
+                      templateVars.orderOpen = orders.filter(o => o.status === 1); // This variable needs to be renamed to orderOpenItems
+                      templateVars.orderActive = userOrder.filter(a => a.status === 2 ? a.id : false); // This variable need to be renamed to orderActiveItem
+                      res.render('pages/index', templateVars);
+                    })
+                    .catch((err) => {
+                      console.error("Error (getOrderItems): ", err.messages);
+                    })
+                  }
+                })
+                .catch((err) => {
+                  console.error("Error (getOrdersByUserId): ", err.messages);
+                })
+            })
+            .catch((err) => {
+              console.log("Error (getUsersById): ", err.messages);
+            })
           })
-        })
           .catch((err) => {
-            console.log("Error: ", err.messages);
+            console.error("Error (getRestaurantById): ", err.messages);
           })
         })
-      })
+        .catch((err) => {
+          console.error("Error (getAddressesById): ", err.messages);
+        })
       })
       .catch((err) => {
         res.render('partials/messages', err.messages);
