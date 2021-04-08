3140f48725e53ecb0a7511eec7a7d8c6efe40f7c
Jacqueline <jlee4332@gmail.com>
2021-04-07 23:41:48 -0400

    css changes

--- db/seeds/05_menu_items.sql
+++ db/seeds/05_menu_items.sql
@@ -1,6 +1,6 @@
 INSERT INTO menu_items (restaurant_id, name, description, image_url, price, prep_time, menu_item_category_ID)
 VALUES(1, 'Henryburger', 'a burger with cheese, lettuce, tomatoes', '/images/menu_items_images/cheeseburger.jpg', 900, 5, 1),
-(1, 'Henryveggie', 'vegan friendly burger with cheese, lettuce, tomatoes', '/images/menu_items_images/veggieburger.jpg', 800, 5, 1),
+(1, 'Henryveggie', 'vegan friendly burger', '/images/menu_items_images/veggieburger.jpg', 800, 5, 1),
 (1, 'Henryfries', 'Henry approved french fries', '/images/menu_items_images/fries.jpg', 300, 4, 2),
 (1, 'Poutine', 'french fries with cheese curds and gravy', '/images/menu_items_images/poutine.jpg', 800, 6, 2 ),
 (1, 'House salad', 'lettuce and veggies - not Henry approved', '/images/menu_items_images/salad.jpg', 700, 3, 2),
