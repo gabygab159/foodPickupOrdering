3ea1a470986b2ae9ce0c95a011cb5891b393ae35
Gabriel <gabriel.jchen@gmail.com>
2021-04-08 20:40:52 +0000

    clean index routes

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -1,5 +1,4 @@
 /*
- * All routes for Menus are defined here
  * Since this file is loaded in server.js into api/users,
  *   these routes are mounted onto /users
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
